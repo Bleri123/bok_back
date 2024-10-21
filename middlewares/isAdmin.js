db19d70ba4b982a12ed1c00bf34e02c07f9d05cc
Bleri123 <bleonnasufi9@gmail.com>
2024-10-21 20:25:14 +0200

    Middlewares done

---
+++ middlewares/isAdmin.js
@@ -0,0 +1,11 @@
+// function
+
+export const isAdmin = (req, res, next) => {
+
+    if(req.user.role_id === 1) {
+        next();
+    }
+    else {
+        res.status(403).json({error: "Access Denied, Admin Only"});
+    }
+}
