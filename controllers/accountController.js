4d872b666a4020cd6f0707592499d4d4ff432883
qema00 <168121699+qema00@users.noreply.github.com>
2024-10-23 21:25:57 +0200

    qema finished fetch account by user id (#19)

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -6,6 +6,18 @@ export const fetchAccount = () => {
   //   talat
 };
 
+export const fetchAccountByUserId = (req, res) => {
+  const user_id = req.params.user_id
+  const sql = "SELECT * FROM accounts WHERE user_id = ?";
+  db.query(sql,[user_id], (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: "Database query error" });
+    }
+    res.json(results);
+  });
+};
+
+
 
 
 export const getallaccount_types = (req, res) => {
@@ -13,6 +25,7 @@ export const getallaccount_types = (req, res) => {
   db.query(sql, (err, results) => {
     if (err) {
       console.log(err)
+
       return res.status(500).json({ error: "Database query error" });
     }
     res.json(results);
--- routes/api.js
+++ routes/api.js
@@ -9,17 +9,19 @@ import {
 import authenticateToken from "../middlewares/authMiddleware.js";
 import { isAdmin } from "../middlewares/isAdmin.js";
 import { isActive } from "../middlewares/isActive.js";
-import { fetchAccount,getallaccount_types } from "../controllers/accountController.js";
-import { fetchAccount } from "../controllers/accountController.js";
+import { fetchAccount, fetchAccountByUserId, getallaccount_types } from "../controllers/accountController.js";
 import { getAlltransaction_types } from "../controllers/transaction_typesController.js";
+
 
 
 // Get all users
 router.get("/users", authenticateToken, isActive, getAllUsers);
 router.get("/users/active-users", getActiveUsers);
 router.get("/accounts", fetchAccount);
+router.get("/accounts/:user_id", fetchAccountByUserId);
 router.get( "/account_types", getallaccount_types); 
 router.get("/transaction_types", getAlltransaction_types);
+
 
 // UPDATE
 router.put("/user/:id", authenticateToken, isAdmin, isActive, updateUserByID);
