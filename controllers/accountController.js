5bec4a7a7e31c18881ad64dc632ce097ed9d4f54
EdiPajaziti <edipajaziti5@gmail.com>
2024-10-23 21:08:03 +0200

    getallaccount_types endpoint & method completed

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,4 +1,20 @@
+import db from "../db.js";
+
 export const fetchAccount = () => {
+
   // qitu ka me punu talati
   //   talat
+};
+
+
+
+export const getallaccount_types = (req, res) => {
+  const sql = "SELECT * FROM account_types";
+  db.query(sql, (err, results) => {
+    if (err) {
+      console.log(err)
+      return res.status(500).json({ error: "Database query error" });
+    }
+    res.json(results);
+  });
 };
--- routes/api.js
+++ routes/api.js
@@ -4,16 +4,18 @@ import {
   getActiveUsers,
   getAllUsers,
   updateUserByID,
+  
 } from "../controllers/usersController.js";
 import authenticateToken from "../middlewares/authMiddleware.js";
 import { isAdmin } from "../middlewares/isAdmin.js";
 import { isActive } from "../middlewares/isActive.js";
-import { fetchAccount } from "../controllers/accountController.js";
+import { fetchAccount,getallaccount_types } from "../controllers/accountController.js";
 
 // Get all users
 router.get("/users", authenticateToken, isActive, getAllUsers);
 router.get("/users/active-users", getActiveUsers);
 router.get("/accounts", fetchAccount);
+router.get( "/account_types", getallaccount_types); 
 // UPDATE
 router.put("/user/:id", authenticateToken, isAdmin, isActive, updateUserByID);
-export default router;
+export default router; 
