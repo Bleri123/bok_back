7ca427e642063576f50b1e8615ee6c9ef623b5b5
TalatQ <talatmustafa476@gmail.com>
2024-10-23 21:28:47 +0200

    talatchanches

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,4 +1,10 @@
-export const fetchAccount = () => {
-  // qitu ka me punu talati
-  //   talat
+import db from "../db.js";
+export const fetchAccount = (req,res) => {
+  const sql = "SELECT * FROM accounts";
+  db.query(sql, (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: "Database query error" });
+    }
+    res.json(results);
+  });
 };
