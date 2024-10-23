3e83b4a33db672298ec4529d8d62792e3bab77dc
Bleri123 <bleonnasufi9@gmail.com>
2024-10-23 20:38:42 +0200

    added fetch transaction_types endpoint & transaction_typesController with method

--- routes/api.js
+++ routes/api.js
@@ -9,11 +9,13 @@ import authenticateToken from "../middlewares/authMiddleware.js";
 import { isAdmin } from "../middlewares/isAdmin.js";
 import { isActive } from "../middlewares/isActive.js";
 import { fetchAccount } from "../controllers/accountController.js";
+import { getAlltransaction_types } from "../controllers/transaction_typesController.js";
 
 // Get all users
 router.get("/users", authenticateToken, isActive, getAllUsers);
 router.get("/users/active-users", getActiveUsers);
 router.get("/accounts", fetchAccount);
+router.get("/transaction_types", getAlltransaction_types);
 // UPDATE
 router.put("/user/:id", authenticateToken, isAdmin, isActive, updateUserByID);
 export default router;
