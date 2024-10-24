e3ab15e2e4d3adb4220903ace5e34ff34214f02a
Elton Rexha <elton.rexha123@outlook.com>
2024-10-24 13:48:22 +0200

    Fix merge, and whitespaces

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,10 +1,4 @@
-
 import db from '../db.js';
-
-export const fetchAccount = () => {
-  // qitu ka me punu talati
-  //   talat
-};
 
 export const deleteAccount = async (req, res) => {
   const id = req.params.id;
--- routes/api.js
+++ routes/api.js
@@ -13,8 +13,6 @@ import { fetchAccount, fetchAccountByUserId, getallaccount_types } from "../cont
 import { getAlltransaction_types } from "../controllers/transaction_typesController.js";
 import { deleteAccount } from '../controllers/accountController.js';
 
-
-
 // Get all users
 router.get("/users", authenticateToken, isActive, getAllUsers);
 router.get("/users/active-users", getActiveUsers);
@@ -22,7 +20,6 @@ router.get("/accounts", fetchAccount);
 router.get("/accounts/:user_id", fetchAccountByUserId);
 router.get( "/account_types", getallaccount_types); 
 router.get("/transaction_types", getAlltransaction_types);
-
 
 // UPDATE
 router.put("/user/:id", authenticateToken, isAdmin, isActive, updateUserByID);
