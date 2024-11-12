13727d63dde39cf78930c3b3b7a66a9d4c976300
Bleri123 <bleonnasufi9@gmail.com>
2024-11-12 20:56:44 +0100

    update getalluser and getaccountbyid

--- routes/account.js
+++ routes/account.js
@@ -1,13 +1,22 @@
 import authenticateToken from "../middlewares/authMiddleware.js";
 import { isAdmin } from "../middlewares/isAdmin.js";
-import { fetchAccount, fetchAccountByUserId } from "../controllers/accountController.js";
-import { deleteAccount } from '../controllers/accountController.js';
-import express from 'express';
+import {
+  fetchAccount,
+  fetchAccountByUserId,
+} from "../controllers/accountController.js";
+import { deleteAccount } from "../controllers/accountController.js";
+import { isActive } from "../middlewares/isActive.js";
+import express from "express";
 const router = express.Router();
 
-
-router.get('/', authenticateToken, fetchAccount);
-router.get('/:user_id', fetchAccountByUserId);
-router.delete('/delete/:id', authenticateToken, isAdmin, deleteAccount);
+router.get("/", authenticateToken, fetchAccount);
+router.get(
+  "/:user_id",
+  authenticateToken,
+  isActive,
+  isAdmin,
+  fetchAccountByUserId
+);
+router.delete("/delete/:id", authenticateToken, isAdmin, deleteAccount);
 
 export default router;
--- routes/user.js
+++ routes/user.js
@@ -2,15 +2,21 @@ import {
   getActiveUsers,
   getAllUsers,
   updateUserByID,
-} from '../controllers/usersController.js';
-import authenticateToken from '../middlewares/authMiddleware.js';
-import { isAdmin } from '../middlewares/isAdmin.js';
-import { isActive } from '../middlewares/isActive.js';
-import express from 'express';
+} from "../controllers/usersController.js";
+import authenticateToken from "../middlewares/authMiddleware.js";
+import { isAdmin } from "../middlewares/isAdmin.js";
+import { isActive } from "../middlewares/isActive.js";
+import express from "express";
 const router = express.Router();
 
-router.get('/', authenticateToken, isActive, getAllUsers);
-router.get('/active-users', authenticateToken, isAdmin, isActive, getActiveUsers);
-router.put('/:id', authenticateToken, isAdmin, isActive, updateUserByID);
+router.get("/", authenticateToken, isActive, isAdmin, getAllUsers);
+router.get(
+  "/active-users",
+  authenticateToken,
+  isAdmin,
+  isActive,
+  getActiveUsers
+);
+router.put("/:id", authenticateToken, isAdmin, isActive, updateUserByID);
 
 export default router;
