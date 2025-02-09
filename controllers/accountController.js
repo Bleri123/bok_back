f8276740b876995f29540f5319585d1938361432
Bleri123 <bleonnasufi9@gmail.com>
2025-02-09 12:23:39 +0100

    adding the deposit feature

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -138,7 +138,7 @@ export const userWithdrawMoney = async (req, res) => {
     const new_transaction_id = transactionResponse?.insertId;
 
     const new_balance = balance - total_with_transfer_fee;
-    await queries.reduceBalance(new_balance, user_id, account_type_id);
+    await queries.updateBalance(new_balance, user_id, account_type_id);
 
     await queries.createAccountTransaction(
       user_account_id,
@@ -151,6 +151,72 @@ export const userWithdrawMoney = async (req, res) => {
     });
   } catch (error) {
     console.error("Error processing withdrawal:", error);
+    return res.status(500).json("Internal server error");
+  }
+};
+
+export const userDepositMoney = async (req, res) => {
+  const user_id = req?.user?.id;
+  const chosen_value = parseFloat(req?.body?.chosen_value);
+  const account_type_id = req?.body?.account_type_id;
+
+  const transaction_type_id = 1;
+  const transaction_fee_id = 1;
+
+  try {
+    const userAccountData = await queries.getUserBalance(
+      user_id,
+      account_type_id
+    );
+    if (userAccountData.length === 0) {
+      return res
+        .status(400)
+        .json({ message: "User must have a valid account to make a deposit" });
+    }
+
+    const { balance: rawBalance, id: user_account_id } = userAccountData[0];
+    const balance = parseFloat(rawBalance);
+    // Fetch transaction fee
+    const transactionFeeData = await queries.getTransactionFee(
+      transaction_type_id
+    );
+
+    if (transactionFeeData.length === 0) {
+      return res
+        .status(400)
+        .json({ message: "Failed to retrieve transaction fee" });
+    }
+
+    const transfer_fee = parseFloat(transactionFeeData[0]?.fixed_fee);
+    const total_with_transfer_fee = chosen_value - transfer_fee;
+
+    // Begin deposit process
+    const transactionResponse = await queries.createNewTransaction(
+      transaction_type_id,
+      transaction_fee_id,
+      total_with_transfer_fee
+    );
+    const new_transaction_id = transactionResponse?.insertId;
+
+    // Since this is an ATM deposit, we don't need to check the balance
+    // We directly proceed to increase the balance
+    const new_balance = total_with_transfer_fee;
+    const total_user_balance = balance + total_with_transfer_fee;
+    await queries.updateBalance(total_user_balance, user_id, account_type_id);
+
+    await queries.createAccountTransaction(
+      user_account_id,
+      new_transaction_id,
+      "DEPOSIT"
+    );
+
+    return res.status(200).json({
+      message: `Successfully deposited: ${total_with_transfer_fee.toFixed(
+        2
+      )}€ (fee included)`,
+    });
+  } catch (error) {
+    console.error("Error processing deposit:", error);
     return res.status(500).json("Internal server error");
   }
 };
